3689bbc93b43df9b50fee97f34cfb6972fd20b9c
Danfall369 <Danfall963@gmail.com>
2023-03-09 12:47:01 -0400

    Popup menu mobile & desk finish

--- js/projectcards.js
+++ js/projectcards.js
@@ -54,35 +54,35 @@ const projectCards = [{
 }];
 
 // Dynamics Cards
-document.querySelector('#grid-boxs').innerHTML = projectCards.map(
-  (info) => `
-    <div class="project-box1">
-    <div class="info-box">
-      <h3 class="box-name">${info.name[0]}</h3>
-      <div class="tags-list">
-        <div id="info-box-tags">
-          <ul id="info-tags">
-            <li class="big-tag">${info.tecnologies[0]}</li>
-            <li class="small-tag">${info.tecnologies[1]}</li>
-            <li class="medium-tag">${info.tecnologies[2]}</li>
-            <li class="small-tag">${info.tecnologies[3]}</li>
-          </ul>
+const gridBox = document.querySelector('#grid-boxs');
+const cards = () => {
+  projectCards.map((dataInfo) => {
+    const projectBox = document.createElement('div');
+    projectBox.classList.add('.project-box1');
+    projectBox.innerHTML = `
+      <div class="project-box1">
+      <div class="info-box">
+        <h3 class="box-name">${dataInfo.name[0]}</h3>
+        <div class="tags-list">
+          <div id="info-box-tags">
+            <ul id="info-tags">
+              <li class="big-tag">${dataInfo.tecnologies[0]}</li>
+              <li class="small-tag">${dataInfo.tecnologies[1]}</li>
+              <li class="medium-tag">${dataInfo.tecnologies[2]}</li>
+              <li class="small-tag">${dataInfo.tecnologies[3]}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="action">
+          <button class="small-buttom" type="button">See project</button>
         </div>
       </div>
-      <div class="action">
-        <button class="small-buttom" type="button">See project</button>
-      </div>
     </div>
-  </div>
-    `,
-)
-  .join('');
-
-for (let i = 0; i <= projectCards.length; i++) {
-  if (i % 2 === 1) {
-    document.querySelectorAll('.project-box1')[i].classList.add('#grid-boxs');
-  }
-}
+      `;
+    gridBox.append(projectBox);
+  });
+};
+cards()
 
 // PopUP menu
 const popUp = document.querySelector('.detail-mobile');
@@ -92,9 +92,10 @@ section.innerHTML = `
 <div class="card-works">
 <div class="img-portfolio">
   <button class="x-button" type="button">x</button>
-  <img src="/Style/Detail-mobile/Snapshoot-Portfolio.svg" alt="Snapshoot">
+  <img class="img-mobile" src="/Style/Detail-mobile/Snapshoot-Portfolio.svg" alt="Snapshoot">
+  <img class="img-desk" src="/Style/Detail-mobile/Snapshoot-Dsk.svg" alt="Snapshoot">
 </div>
-<div class="Primary-text">
+<div class="primary-text">
   <div class="project-name">
     <h3 class="primarytxt">Keeping track of hundreds of components</h3>
   </div>
@@ -117,11 +118,11 @@ section.innerHTML = `
 </div>
 <div class="actions">
   <div class="greenbb">
-    <a href="https://danfall369.github.io/Microverse-Portfolio/" class="a-btn">See Live <img class="btn-img"
+    <a href="https://danfall369.github.io/Microverse-Portfolio/" class="a-btn" target="_blank">See Live <img class="btn-img"
         src="/Style/Detail-mobile/ext-ico.svg" alt="ext-ico"></a>
   </div>
   <div class="greenbb">
-    <a href="https://github.com/Danfall369/Microverse-Portfolio" class="a-btn">See Source <img class="btn-img"
+    <a href="https://github.com/Danfall369/Microverse-Portfolio" class="a-btn" target="_blank">See Source <img class="btn-img"
         src="/Style/Detail-mobile/GitHub.svg" alt="ext-ico"></a>
   </div>
 </div>
@@ -133,7 +134,7 @@ PopUpbutton.forEach((button) => {
   const detail = document.querySelector('.detail-mobile');
   button.addEventListener('click', () => {
     detail.style.display = 'block';
-  })
+  });
 });
 
 const closePop = document.querySelector('.x-button');
